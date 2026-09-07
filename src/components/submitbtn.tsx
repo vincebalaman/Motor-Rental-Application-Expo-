9196ef9e00ae237795363be711ff21a43a45a891
define <michealtimjoseph.enriquez@gmail.com>
2026-09-08 04:11:22 +0800

    Fix unresolved codes
    Change "CustomButtonProps" to SubmitButtonProps matched to interface.

--- src/components/submitbtn.tsx
+++ src/components/submitbtn.tsx
@@ -7,12 +7,7 @@ interface SubmitButtonProps {
   style?: StyleProp<ViewStyle>;
 }
 
-export default function CustomButton({
-  title,
-  onPress,
-  isSubmitting = false,
-  style,
-}: CustomButtonProps) {
+export default function SubmitButton({title, onPress, isSubmitting = false, style,}: SubmitButtonProps) {
   return (
     <Pressable
       style={[styles.button, style, isSubmitting && styles.buttonDisabled]}
